648c55066fc9158eeddd2af331efbee59b1741b9
Ly Tran <lyttt.dev@gmail.com>
2021-10-14 07:50:03 +0700

    View holder

--- RecylerViewExample/app/src/main/java/dev/lytran/adapter/BeerAdapter.java
+++ RecylerViewExample/app/src/main/java/dev/lytran/adapter/BeerAdapter.java
@@ -1,45 +1,51 @@
 package dev.lytran.adapter;
 
 import android.app.Activity;
-import android.content.Context;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
-
-import java.util.List;
+import java.util.ArrayList;
 
 import dev.lytran.recylerviewexample.R;
 import dev.lytran.model.Beer;
 
-public class BeerAdapter extends RecyclerView.Adapter {
+public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
 
+    Activity context;
+    ArrayList<Beer> products;
+
+    public BeerAdapter(Activity context, ArrayList<Beer> products) {
+        this.context = context;
+        this.products = products;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View customView = inflater.inflate(R.layout.custom_item, parent, false);
+
+        return new ViewHolder(customView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imvThumb.setImageResource(products.get(position).getProductThumb());
+        holder.txtInfo.setText(products.get(position).getProductName() + "-" + products.get(position).getProductPrice());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return products.size();
     }
 
-    private class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView imvThumb;
         TextView txtInfo;
 
