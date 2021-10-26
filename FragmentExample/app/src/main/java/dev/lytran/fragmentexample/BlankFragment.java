ace0c944bd1f861bebc84d63cf26b0fb48479dd7
Ly Tran <lyttt.dev@gmail.com>
2021-10-26 07:04:28 +0700

    Fragment

---
+++ FragmentExample/app/src/main/java/dev/lytran/fragmentexample/BlankFragment.java
@@ -0,0 +1,19 @@
+package dev.lytran.fragmentexample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
