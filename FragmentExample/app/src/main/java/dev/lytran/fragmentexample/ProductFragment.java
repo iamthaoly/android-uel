654b1652e69056a9d6174a73a1348203b0cfe080
Ly Tran <lyttt.dev@gmail.com>
2021-10-28 08:05:35 +0700

    Fragment

--- FragmentExample/app/src/main/java/dev/lytran/fragmentexample/ProductFragment.java
+++ FragmentExample/app/src/main/java/dev/lytran/fragmentexample/ProductFragment.java
@@ -12,18 +12,33 @@ import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
+import dev.lytran.adapter.ProductAdapter;
 import dev.lytran.model.Product;
 
 public class ProductFragment extends Fragment {
 
     ListView lvProduct ;
     ArrayList<Product> products;
+    ProductAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.product_fragment, container, false);
-        
+        adapter = new ProductAdapter(getContext(), R.layout.item_listview, initData());
+        lvProduct.setAdapter(adapter);
         return v;
+    }
+
+    private ArrayList<Product> initData() {
+        products = new ArrayList<>();
+        products.add(new Product("Heineken", 19003 , R.drawable.sapporo));
+        products.add(new Product("Hanoi", 19003 , R.drawable.hanoi));
+        products.add(new Product("Sing", 19003 , R.drawable.beer333));
+        products.add(new Product("Ken", 19003 , R.drawable.larue));
+        products.add(new Product("Tiger", 19003 , R.drawable.sapporo));
+        products.add(new Product("Heineken", 19003 , R.drawable.saigon));
+
+        return  products;
     }
 }
