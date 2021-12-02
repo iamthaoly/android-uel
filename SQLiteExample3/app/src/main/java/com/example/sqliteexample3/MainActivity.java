f5900258264403bac834472593fa808f1b7e01b0
Ly Tran <lyttt.dev@gmail.com>
2021-12-02 07:49:15 +0700

    sqlite 3

--- SQLiteExample3/app/src/main/java/com/example/sqliteexample3/MainActivity.java
+++ SQLiteExample3/app/src/main/java/com/example/sqliteexample3/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.sqliteexample3;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +20,9 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +33,43 @@ public class MainActivity extends AppCompatActivity {
     BottomSheetDialog sheetDialog;
 
     LinearLayout sheetCamera, sheetGallery;
+    boolean isCamera;
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        linkViews() ;
+        linkViews();
+        createSheetDialog();
         addEvents();
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    if (isCamera) {
+                        Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
+                        imvPhoto.setImageBitmap(bitmap);
+                    }
+                    else {
+                        Uri uri = result.getData().getData();
+                        if (uri != null) {
+                            try {
+                                InputStream inputStream = getContentResolver().openInputStream(uri);
+                                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                                imvPhoto.setImageBitmap(bitmap);
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+
+                }
+            }
+        });
     }
 
     private void addEvents() {
@@ -35,12 +77,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Open bottom sheet
-                openSheetDialog();
+                sheetDialog.show();
             }
         });
     }
 
-    private void openSheetDialog() {
+    private void createSheetDialog() {
         if (sheetDialog == null) {
             View view = LayoutInflater.from(this).inflate(R.layout.bottom_sheet, null);
             sheetCamera = view.findViewById(R.id.sheetOpenCamera);
@@ -49,6 +91,10 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     // Open camera
+                    isCamera = true;
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    activityResultLauncher.launch(intent);
+                    sheetDialog.dismiss();
 
                 }
             });
@@ -56,6 +102,11 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     // Open gallery
+                    isCamera = false;
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    activityResultLauncher.launch(intent);
+                    sheetDialog.dismiss();
                 }
             });
             sheetDialog = new BottomSheetDialog(this);
