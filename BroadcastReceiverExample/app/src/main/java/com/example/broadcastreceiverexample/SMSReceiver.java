6dc698f32be084891cd1f645ab358aef8bdb38fc
Ly Tran <lyttt.dev@gmail.com>
2021-12-09 09:11:40 +0700

    broadcast receiver

--- BroadcastReceiverExample/app/src/main/java/com/example/broadcastreceiverexample/SMSReceiver.java
+++ BroadcastReceiverExample/app/src/main/java/com/example/broadcastreceiverexample/SMSReceiver.java
@@ -3,10 +3,36 @@ package com.example.broadcastreceiverexample;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class SMSReceiver extends BroadcastReceiver {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy", Locale.getDefault());
     @Override
     public void onReceive(Context context, Intent intent) {
-        
+        // Logic for reading incoming SMS
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            Object[] arrMessage = (Object[]) bundle.get("pdus");
+            if (arrMessage != null) {
+                String content, phone, formattedTime;
+                long time;
+                byte[] bytes;
+                for (Object aMsg: arrMessage) {
+                    bytes = (byte[]) aMsg;
+                    SmsMessage message = SmsMessage.createFromPdu(bytes);
+                    content = message.getMessageBody();
+                    phone = message.getDisplayOriginatingAddress();
+                    time = message.getTimestampMillis();
+                    formattedTime = dateFormat.format(time);
+
+                    Toast.makeText(context, "Phone: " + phone + "\n" + "Time: " + formattedTime + "\n" + "Content: " + content, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
