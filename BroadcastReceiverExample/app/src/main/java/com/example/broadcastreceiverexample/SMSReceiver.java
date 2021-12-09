7a306d350111027aea260d9deeec304b01ac210c
Ly Tran <lyttt.dev@gmail.com>
2021-12-09 08:54:28 +0700

    broadcast receiver

---
+++ BroadcastReceiverExample/app/src/main/java/com/example/broadcastreceiverexample/SMSReceiver.java
@@ -0,0 +1,12 @@
+package com.example.broadcastreceiverexample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
